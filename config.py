eb775a8361fe42078791defa5cbff882a1aaf2d1
gburlew <164833372+gburlew@users.noreply.github.com>
2024-05-07 22:55:10 -0700

    fixing file path

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import pandas as pd
 import json
 from pathlib import Path
 
-locations_age_csv = Path("locations-age.csv")
-vaccinations_age_csv = Path("vaccinations-by-age-group.csv")
-vaccinations_manufacturer_csv = Path("vaccinations-by-manufacturer.csv")
+locations_age_csv = Path("../Resources/locations-age.csv")
+vaccinations_age_csv = Path("../Resources/vaccinations-by-age-group.csv")
+vaccinations_manufacturer_csv = Path("../Resources/vaccinations-by-manufacturer.csv")
