6f224b04bc646ed82ce8e4a0fba01d9f6efed951
gburlew <164833372+gburlew@users.noreply.github.com>
2024-05-07 22:58:51 -0700

    added file path

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ from pathlib import Path
 locations_age_csv = Path("../Resources/locations-age.csv")
 vaccinations_age_csv = Path("../Resources/vaccinations-by-age-group.csv")
 vaccinations_manufacturer_csv = Path("../Resources/vaccinations-by-manufacturer.csv")
+full_data_csv = Path("../Resources/full_data.csv")
